4310498cccf6dd6b9cb9e3ce82d2fa16bb1ed521
David Vloedman <djvloedman@gmail.com>
2020-02-20 12:28:38 -0800

    Add node-sass

--- src/app.js
+++ src/app.js
@@ -1,10 +1,51 @@
 import React from "react";
+import Superagent from "superagent";
+import ReactJson from "react-json-view";
+
+import Form from "./components/form/form";
+import Header from "./components/header/header";
+
+import "./app.scss";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "",
+      method: "get",
+      count: 0,
+      results: []
+    };
+  }
+
+  formSubmit = e => {
+    e.preventDefault();
+    Superagent(this.state.method, this.state.url).then(res => {
+      this.setState({
+        count: res.body.count,
+        results: res.body.results
+      });
+    });
+  };
+
+  setMethod = method => {
+    this.setState({ method });
+  };
+
+  setUrl = e => {
+    this.setState({ url: e.target.value });
+  };
+
   render() {
     return (
       <>
-        <h1>Gonna be some resty here soon! Stay tuned...</h1>
+        <Header />
+        <Form
+          onSubmit={this.formSubmit}
+          setMethod={this.setMethod}
+          setUrl={this.setUrl}
+        />
+        <ReactJson src={this.state.results} />
       </>
     );
   }
